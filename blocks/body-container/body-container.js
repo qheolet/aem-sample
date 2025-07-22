26f66189ff5c18f26106247802e12a8acf11dafc
DOITT\juduran <juduran@oti.nyc.gov>
2025-07-22 08:19:05 -0400

    Refactor Body Container component: remove unnecessary template properties

--- blocks/body-container/body-container.js
+++ blocks/body-container/body-container.js
@@ -1,15 +1,3 @@
 export default function decorate(block) {
-  const title = document.createElement('div');
-  title.className = 'body-container-title';
-  title.textContent = 'Title goes here';
-
-  const text1 = document.createElement('div');
-  text1.className = 'body-container-text';
-  text1.textContent = 'First text goes here';
-
-  const text2 = document.createElement('div');
-  text2.className = 'body-container-text';
-  text2.textContent = 'Second text goes here';
-
-  block.append(title, text1, text2);
+debugger;
 }
